5f70e28ae20fdcf1896caf99c05149b604a29736
gaoxin.wei <gaoxin.wei@dianping.com>
2017-11-29 21:44:47 +0800

    add

--- MyFunctor.hs
+++ MyFunctor.hs
@@ -10,3 +10,7 @@ instance MyFunctor Maybe where
 instance MyFunctor BinSearchTree.Tree where 
 	fmap2 f EmptyTree = EmptyTree
 	fmap2 f (Node a left right) =  Node (f a) (fmap2 f left) (fmap2 f right)
+
+instance MyFunctor (Either Int) where
+	fmap2 f (Left x) = Left x
+	fmap2 f (Right x) = Right (f x)
